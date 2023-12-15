12dc22e86333a8d0c8d12c8424d1c0ab92205b55
sharadaade <aadesharad@gmail.com>
2023-12-15 21:36:28 +0530

    bro practice first

---
+++ c-bro-practice.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+int main(void)
+{
+    // variable => Allocatd space in memory to store a value
+    // int a; // declaration
+    // a = 10; // initialization
+    // int z =  35, m; // declaration + initialization
+    // int old_age = 22;
+    // int newAge;
+    // int i, j, k;
+    // i = j = k = 7;
+    // printf("%d\n",i);
+    // printf("%d\n",j);
+
+
+    // Even or odd number = Even=> 1 | Odd => 0
+    // int num;
+    // printf("Enter a number : ");
+    // scanf("%d",&num);
+    // printf("%d",num % 2 == 0);
+
+    // Conditional 
+    // if-else && switch
+
+    // if-else 
+
+    // int age;
+    // printf("Enter your age : ");
+    // scanf("%d",&age);
+    // if(age >= 18)
+    // {
+    //     printf("You are adult");
+    // }
+    // else if(age > 13 && age < 18)
+    // {
+    //     printf("You are teenage");
+    // }
+    // else
+    // {
+    //     printf("You are cild");
+    // }
+
+    // Ternary operator
+    // int myAge;
+    // printf("Enter your age : ");
+    // scanf("%d",&myAge);
+
+    // myAge >= 18 ? printf("Adult") : (myAge>13 && myAge<18) ? printf("Teenager") : printf("Child");
+
+
+//     int month;
+//     printf("Enter the month(1-12) : ");
+//     scanf("%d",&month);
+
+//     switch(month)
+//     {
+//         case 1:
+//             printf("January");
+//             break;
+//         case 2:
+//             printf("February");
+//             break;
+//         case 3:
+//             printf("March");
+//             break;
+//         case 4:
+//             printf("April");
+//             break;
+//         case 5:
+//             printf("May");
+//             break;
+//         case 6:
+//             printf("June");
+//             break;
+//         case 7:
+//             printf("July");
+//             break;
+//         case 8:
+//             printf("August");
+//             break;
+//         case 9:
+//             printf("September");
+//             break;
+//         case 10:
+//             printf("October");
+//             break;
+//         case 11:
+//             printf("November");
+//             break;
+//         case 12:
+//             printf("December");
+//             break;
+//         default:
+//             printf("Not valid Month");
+//     }
+
+// Loop 1)for 2)while 3)do-while
+
+    // for(int i=10; i>=1;i--)
+    // {
+    //     printf("%d\n",i);
+    // }
+
+    // for(int j=1;j<=10;j++)
+    // {
+    //     printf("%d\n",j);
+    // }
+
+    // for(int i=1;i<=10;i++)
+    // {
+    //     printf("Hello %d\n",i);
+    // }
+
+}
+
