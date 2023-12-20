b90ae134a972560b658bd038bad55a7546ffefb0
sharadaade <aadesharad@gmail.com>
2023-12-20 17:46:31 +0530

    ok

--- om.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-int main(void)
-{
-    int i;
-    printf("Enter age :");
-    scanf("%d",&i);
-    printf("You are %d year old",i);
-}
---
+++ while-a-to-z.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    // Find ASCII value
+    // char cha = 'a';
+    // printf("%d",cha);
+    // char chz = 'z';
+    // printf("%d",chz);
+
+    // a = 97;
+    // z = 122;
+    int i = 97;
+    while (i <= 122 )
+    {
+        printf("%c\n",i);
+        i++;
+    }
+    
+}
